44fa4925ff286df51f4c9e31a4a8b78c69a5b591
your name <sriwljitthitima@gmail.com>
2023-08-05 19:26:18 +0700

    up

--- Homework/HW01/hw01-3-basic-input-output.cpp
+++ Homework/HW01/hw01-3-basic-input-output.cpp
@@ -13,3 +13,15 @@
     Output:
         ABD DEFs TC, RMUTL, Chiang Mai, Thailand
 */
+#include <stdio.h>
+int main(){
+    char Fname[50];
+    char Lname[50];
+    printf("Enter your First Name: ");
+    scanf("%s",Fname);
+    printf("Enter your Last Name");
+    scanf("%s",Lname);
+    printf("%s %ss TC,RMUTL,Chiang Mai,Thailand",Fname,Lname);
+    return 0 ;
+}
+
